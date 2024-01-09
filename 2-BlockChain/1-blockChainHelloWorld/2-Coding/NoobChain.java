152e363b910ea301e88cd4a82df7865f6d7c6434
zeng <2486851360@qq.com>
2024-01-09 16:28:17 +0800

    After mining

--- 2-BlockChain/1-blockChainHelloWorld/2-Coding/NoobChain.java
+++ 2-BlockChain/1-blockChainHelloWorld/2-Coding/NoobChain.java
@@ -2,25 +2,35 @@ import java.util.ArrayList;
 
 import com.google.gson.GsonBuilder;
 
-//12e
-
 public class NoobChain {
+	public static int difficulty = 3;
 
-	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
-	public static void main(String[] args) {	
-		//add our blocks to the blockchain ArrayList:
-		blockchain.add(new Block("Hi im the first block", "0"));		
-		blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash)); 
-		blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
-		
-		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+	public static void main(String[] args) {
+		// add our blocks to the blockchain ArrayList:
+		blockchain.add(new Block("Sa si su se so", "0"));
+		System.out.println("Trying to mine sa si su se so... ");
+		blockchain.get(0).mineBlock(difficulty);
+
+		blockchain.add(new Block("日本語を勉強する", blockchain.get(blockchain.size() - 1).hash));
+		System.out.println("Trying to mine 日本語を勉強する... ");
+		blockchain.get(1).mineBlock(difficulty);
+
+		blockchain.add(new Block("氷菓はぜんの一番すきの番組です", blockchain.get(blockchain.size() - 1).hash));
+		System.out.println("Trying to mine 氷菓はぜんの一番すきの番組です... ");
+		blockchain.get(2).mineBlock(difficulty);
+
+		System.out.println("\nBlockchain is Valid: " + isChainValid());
+
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 		System.out.println(blockchainJson);
 	}
 
 	public static Boolean isChainValid() {
 		Block currentBlock;
 		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
 		// loop through blockchain to check hashes:
 		for (int i = 1; i < blockchain.size(); i++) {
@@ -34,6 +44,11 @@ public class NoobChain {
 			// compare previous hash and registered previous hash
 			if (!previousBlock.hash.equals(currentBlock.previousHash)) {
 				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			// check if hash is solved
+			if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
 				return false;
 			}
 		}
