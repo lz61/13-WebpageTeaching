10f3e3d1f89e62c927607c9115d2564d91e17077
zeng <2486851360@qq.com>
2024-01-09 17:36:58 +0800

    Day 2 : Finished Testing Wallets and Signatures

--- 2-BlockChain/1-blockChainHelloWorld/2-Coding/NoobChain.java
+++ 2-BlockChain/1-blockChainHelloWorld/2-Coding/NoobChain.java
@@ -1,13 +1,35 @@
+import java.security.Security;
 import java.util.ArrayList;
 
 import com.google.gson.GsonBuilder;
 
 public class NoobChain {
-	public static int difficulty = 3;
+	public static int difficulty = 1000;
 
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
+	public static Wallet walletA;
+	public static Wallet walletB;
+
 	public static void main(String[] args) {
+		//Setup Bouncey castle as a Security Provider
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); 
+		//Create the new wallets
+		walletA = new Wallet();
+		walletB = new Wallet();
+		//Test public and private keys
+		System.out.println("Private and public keys:");
+		System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+		System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+		//Create a test transaction from WalletA to walletB 
+		Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+		transaction.generateSignature(walletA.privateKey);
+		//Verify the signature works and verify it from the public key
+		System.out.println("Is signature verified");
+		System.out.println(transaction.verifySignature());
+	}
+
+	public static void mainForDay1(){
 		// add our blocks to the blockchain ArrayList:
 		blockchain.add(new Block("Sa si su se so", "0"));
 		System.out.println("Trying to mine sa si su se so... ");
@@ -25,7 +47,9 @@ public class NoobChain {
 
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 		System.out.println(blockchainJson);
+
 	}
+
 
 	public static Boolean isChainValid() {
 		Block currentBlock;
