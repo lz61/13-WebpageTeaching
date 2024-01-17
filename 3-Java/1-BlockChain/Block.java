a5f7b4e31b9d73719103fc269d5477a8779adcf6
zeng <2486851360@qq.com>
2024-01-17 20:08:15 +0800

    normal push

--- 3-Java/1-BlockChain/Block.java
+++ 3-Java/1-BlockChain/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String previousHash;
     public String data; // stores a single message
     private Long timestamp; // number of milliseconds since 1970/1/1
+    private int nonce;
 
     // f(current data,previous data)=current hash
 
@@ -23,6 +24,7 @@ public class Block {
         	String calculatedhash = StringUtil.applySha256( 
 			previousHash +
 			Long.toString(this.timestamp) +
+            Integer.toString(nonce)+
 			data 
 			);
 	        return calculatedhash;
@@ -30,6 +32,23 @@ public class Block {
 
     public static void main(String[] args) {
         
+    }
+
+    public void mineBlock(int difficulty) {
+        // difficulty:5 target: 00000
+        // difficulty:3 target: 000
+        String target = new String(new char[difficulty]).replace('\0', '0'); // Create a string with difficulty * "0"
+        // 取出hash前difficulty位
+        // hash:12424324 difficulty: 3 
+        // 124
+        // 000: [0-9]
+        // 1/1000
+        while (!hash.substring(0, difficulty).equals(target)) {
+            // nonce:0 1 2 3 
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 
     public static void mainDay1(){
--- 3-Java/1-BlockChain/NoobChain.java
+++ 3-Java/1-BlockChain/NoobChain.java
@@ -1,12 +1,38 @@
 import java.util.ArrayList;
 import com.google.gson.GsonBuilder;
 
+
 public class NoobChain {
     // 数组: blockchain
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
+    public static int difficulty = 6;
+
     // 三块block: 放到数组里面
     public static void main(String[] args) {
+        // add our blocks to the blockchain ArrayList:
+
+        blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
+        System.out.println(blockchainJson);
+
+    }
+
+    public static void mainDay1(){
         // add our blocks to the blockchain ArrayList:
         blockchain.add(new Block("Hyouka", "0"));
         blockchain.add(new Block("Sakura", blockchain.get(blockchain.size() - 1).hash ));
@@ -19,6 +45,7 @@ public class NoobChain {
 
         blockchain.get(0).data = "Hyouka";
         System.out.println("Blockchain is valid: " + isChainValid());
+
     }
 
     public static void mainFirst(){
@@ -38,6 +65,7 @@ public class NoobChain {
     public static Boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         currentBlock=blockchain.get(0);
         if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
@@ -57,6 +85,11 @@ public class NoobChain {
             // compare previous hash and registered previous hash
             if (!previousBlock.hash.equals(currentBlock.previousHash)) {
                 System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            // check if hash is solved
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
                 return false;
             }
         }
