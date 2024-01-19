02bb82583ad00839d8b25b71e6f263a5df26954c
enoshimakunn <20307140002@fudan.edu.cn>
2024-01-19 13:59:25 +0800

    1.19

--- 1-frontend/2-JAVA/NoobChain.java
+++ 1-frontend/2-JAVA/NoobChain.java
@@ -1,12 +1,35 @@
+import java.security.Security;
 import java.util.ArrayList;
+import java.util.Base64;
 import com.google.gson.GsonBuilder;
 
 public class NoobChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
     public static int difficulty = 5;
+    public static Wallet walletA;
+	public static Wallet walletB;
 
     public static void main(String[] args) {	
+		//Setup Bouncey castle as a Security Provider
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); 
+		//Create the new wallets
+		walletA = new Wallet();
+		walletB = new Wallet();
+		//Test public and private keys
+		System.out.println("Private and public keys:");
+		System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+		System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+		//Create a test transaction from WalletA to walletB 
+		Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+		transaction.generateSignature(walletA.privateKey);
+		//Verify the signature works and verify it from the public key
+		System.out.println("Is signature verified");
+		System.out.println(transaction.verifiySignature());
+		
+	}
+
+    public static void mainDay2(String[] args) {	
 		//add our blocks to the blockchain ArrayList:
 		
 		blockchain.add(new Block("Mahler", "0"));
