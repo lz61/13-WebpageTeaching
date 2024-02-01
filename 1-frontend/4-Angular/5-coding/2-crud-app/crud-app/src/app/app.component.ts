7fdba8ba20e882feca1c57a6fde90bd8e5ee254c
zeng <2486851360@qq.com>
2024-02-01 12:38:59 +0800

    Teaching Code here

--- 1-frontend/4-Angular/5-coding/2-crud-app/crud-app/src/app/app.component.ts
+++ 1-frontend/4-Angular/5-coding/2-crud-app/crud-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-app';
+
+  constructor(public dialog: MatDialog) { 
+  }
+
+  openDialog(): void {
+    this.dialog.open(EmpAddEditComponent);
+  }
 }
--- 1-frontend/4-Angular/5-coding/2-crud-app/crud-app/src/app/app.module.ts
+++ 1-frontend/4-Angular/5-coding/2-crud-app/crud-app/src/app/app.module.ts
@@ -7,10 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpAddEditComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +25,10 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
