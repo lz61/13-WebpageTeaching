8c0676de5369e8e4286f50e1b7724c7e45e2c548
lz61 <71430392+lz61@users.noreply.github.com>
2025-02-02 16:31:58 +0800

    Local Push

--- 1-frontend/3-JS/11-code/myScript.js
+++ 1-frontend/3-JS/11-code/myScript.js
@@ -1,8 +1,6 @@
-var i=1;
-for(var t=1;t<10;t++){
-    if (i == 1) {
-        console.log("134");
-        break;
-        console.log("1234");
-    }
+var x = [1,2];
+function changeX(x){
+    x[0]=2;
 }
+changeX(x);
+console.log(x[0]);
